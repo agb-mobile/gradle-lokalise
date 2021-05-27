ccd35696b154822edfae0978fd370dae6f11d7c9
Gasper Kojek <gasper.kojek@gmail.com>
2021-05-27 09:32:28 +0200

    Fixed coroutines as they now don't need to rely on Deferred

--- plugin/src/main/java/si/kamino/gradle/lokalise/api/LokaliseService.kt
+++ plugin/src/main/java/si/kamino/gradle/lokalise/api/LokaliseService.kt
@@ -1,19 +1,17 @@
 package si.kamino.gradle.lokalise.api
 
-import kotlinx.coroutines.Deferred
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.*
 import si.kamino.gradle.lokalise.api.model.request.ExportFileRequest
 import si.kamino.gradle.lokalise.api.model.response.ExportFile
 
-
 interface LokaliseService {
 
     @POST("{projectId}/files/download")
-    fun exportFiles(@Header("X-Api-Token") apiKey: String, @Path("projectId") projectId: String, @Body export: ExportFileRequest): Deferred<ExportFile>
+    suspend fun exportFiles(@Header("X-Api-Token") apiKey: String, @Path("projectId") projectId: String, @Body export: ExportFileRequest): ExportFile
 
     @GET @Streaming
-    fun downloadFile(@Url url: String): Deferred<Response<ResponseBody>>
+    suspend fun downloadFile(@Url url: String): Response<ResponseBody>
 
 }
