13ce57c5dbf3097718825e73d2450fe3c6800980
Gasper Kojek <gasper.kojek@gmail.com>
2022-04-20 15:19:51 +0200

    Fixed changes to lokalise API regarding `directoryPrefix`.

--- plugin/src/main/java/si/kamino/gradle/lokalise/task/DownloadTask.kt
+++ plugin/src/main/java/si/kamino/gradle/lokalise/task/DownloadTask.kt
@@ -10,6 +10,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import si.kamino.gradle.lokalise.api.LokaliseService
 import si.kamino.gradle.lokalise.api.model.request.ExportFileRequest
@@ -29,25 +30,39 @@ abstract class DownloadTask @Inject constructor(
 
     @TaskAction
     fun download() {
-        runBlocking {
-            val apiService = initApiService()
-            val exportFiles = apiService.exportFiles(
-                baseExtension.token!!, baseExtension.id!!,
-                ExportFileRequest(indentation = baseExtension.indentation,
-                    exportEmptyAs = baseExtension.exportEmptyAs,
-                    pluralFormat = baseExtension.pluralFormat,
-                    includeComments = baseExtension.includeComments,
-                    replaceBreaks = baseExtension.replaceBreaks,
-                    filterData = baseExtension.filterData,
-                    filterLangs = baseExtension.filterLangs
+        try {
+            runBlocking {
+                val apiService = initApiService()
+                val exportFiles = apiService.exportFiles(
+                    baseExtension.token!!, baseExtension.id!!,
+                    ExportFileRequest(
+                        indentation = baseExtension.indentation,
+                        exportEmptyAs = baseExtension.exportEmptyAs,
+                        pluralFormat = baseExtension.pluralFormat,
+                        includeComments = baseExtension.includeComments,
+                        replaceBreaks = baseExtension.replaceBreaks,
+                        filterData = baseExtension.filterData,
+                        filterLangs = baseExtension.filterLangs,
+                        directoryPrefix = baseExtension.directoryPrefix
+                    )
                 )
-            )
-            val fileResponse = apiService.downloadFile(exportFiles.bundleUrl)
-            val byteStream = fileResponse.body()!!.byteStream()
+                val fileResponse = apiService.downloadFile(exportFiles.bundleUrl)
+                val byteStream = fileResponse.body()!!.byteStream()
 
-            byteStream.use {
-                it.copyTo(outputFile.asFile.get().outputStream())
+                byteStream.use {
+                    it.copyTo(outputFile.asFile.get().outputStream())
+                }
             }
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val responseStr = e.response()?.body()?.toString() ?: e.response()?.errorBody()?.string()
+                println("HTTP Error: Code ${e.code()} message='${e.message()}' ${e.response()}")
+                responseStr?.let { println("\tresponse message: $it") }
+                e.cause?.let { println("\tcause $it") }
+            } else {
+                println(e.message)
+            }
+            throw e
         }
     }
 
@@ -59,6 +74,7 @@ abstract class DownloadTask @Inject constructor(
         val contentType = "application/json".toMediaType()
         val converterFactory = json.asConverterFactory(contentType)
 
+        // Note: To see the logs, you need to run gradle with at least `--info` logging level.
         val logging = HttpLoggingInterceptor()
         logging.level = HttpLoggingInterceptor.Level.BODY
 
